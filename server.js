ee0dc8d32d6596b5af6447513c7d13a60cc51c35
thingiverse12 <vinciel@icloud.com>
2025-07-12 18:01:41 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('Användare ansluten:', socket.id);
+
+  socket.on('offer', (data) => {
+    socket.broadcast.emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.broadcast.emit('answer', data);
+  });
+
+  socket.on('candidate', (data) => {
+    socket.broadcast.emit('candidate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Användare kopplad från:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server lyssnar på port ${PORT}`);
+});
